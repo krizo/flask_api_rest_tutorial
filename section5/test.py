b4d0085879ee720cb08feccce7dadd7951b69e13
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-10-09 17:57:33 +0200

    Create tables

--- section5/test.py
+++ section5/test.py
@@ -12,20 +12,20 @@ connection = sqlite3.connect(DATABASE_FILE)
 
 cursor = connection.cursor()
 
-create_table = "CREATE TABLE users (id int, username text, password text)"
-cursor.execute(create_table)
+# create_table = "CREATE TABLE users (id int, username text, password text)"
+# cursor.execute(create_table)
 
-users = [
-    (1, 'kriz', 'asdf'),
-    (2, 'emi', 'easy')
-]
+# users = [
+#     (1, 'kriz', 'asdf'),
+#     (2, 'emi', 'easy')
+# ]
+#
+# insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+# cursor.executemany(insert_query, users)
 
-insert_query = "INSERT INTO users VALUES (?, ?, ?)"
-cursor.executemany(insert_query, users)
+# query = "SELECT * FROM users"
 
-query = "SELECT * FROM users"
-
-[print(row) for row in cursor.execute(query)]
+# [print(row) for row in cursor.execute(query)]
 
 connection.commit()
 connection.close()
