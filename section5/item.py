0c0576c64b5727b106989b2852e9d568e7b1459c
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-10-12 22:34:31 +0200

    Put item

--- section5/item.py
+++ section5/item.py
@@ -36,6 +36,26 @@ class Item(Resource):
         if row:
             return { 'item': {'name': row[0], 'price': row[1]}}
 
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES(?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
+        return item, 201
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
 
     @jwt_required()
     def post(self, name):
@@ -44,14 +64,10 @@ class Item(Resource):
 
         payload = Item.parser.parse_args()
         item = {"name": name, "price": payload["price"]}
-
-        insert = "INSERT INTO items VALUES(?, ?)"
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        cursor.execute(insert, (item['name'], item['price']))
-
-        connection.commit()
-        connection.close()
+        try:
+            self.insert(item)
+        except:
+            return {"message": "An error occured while inserting {} item".format(item)}, 500
 
         return item, 201
 
@@ -60,25 +76,32 @@ class Item(Resource):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
         query = "DELETE FROM items where name=?"
+
         cursor.execute(query, (name, ))
 
         connection.commit()
         connection.close()
 
-        return {"message": {"Item {} deleted".format(name)}}
+        return {"message": "Item {} deleted".format(name)}
 
 
     @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
-        item = find_item(name)
-        if item is None:
-            item = {"name": data["name"], "price": data["price"]}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
+        item = self.find_by_name(name)
+        updated_item = { 'name': name, 'price': data['price']}
 
+        if updated_item is None:
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message": "An error occured on inserting the item {}".format(item)}, 500
+        else:
+            try:
+                self.update(updated_item)
+            except:
+                return {"message": "An error occured on inserting the item {}".format(item)}, 500
+        return updated_item
 
 class ItemList(Resource):
     @jwt_required()
