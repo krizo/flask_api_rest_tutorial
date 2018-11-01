5ffcc11680ec8deb4920829fe95482146235049c
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-11-01 21:28:39 +0100

    Store model and resource

--- section6/app.py
+++ section6/app.py
@@ -4,6 +4,7 @@ from flask_jwt import JWT
 from section6.security import authenticate, identity
 from section6.resources.user import UserRegister
 from section6.resources.item import Item, ItemList
+from section6.resources.store import Store, StoreList
 import os
 
 app = Flask(__name__)
@@ -24,6 +25,8 @@ jwt = JWT(app, authentication_handler=authenticate, identity_handler=identity)
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 
 if __name__ == "__main__":
     from section6.db import db
